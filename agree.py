0d380d898001829060c2402f1f32e39a758c311d
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-04-27 07:43:51 -0400

    agree.py

--- agree.py
+++ agree.py
@@ -1,5 +1,5 @@
 s = input ("agree:")
 if s == "y" or s =="Y":
     print("agreed")
-if s == "n" or s == "N":
+elif s == "n" or s == "N":
     print("not agreed")  
---
+++ agree_otherway.py
@@ -0,0 +1,5 @@
+s = input ("do you agree")
+if s in ["y","Y"]:
+   print("agreed")
+if s in ["N","n"]:
+   print("not agreed")
