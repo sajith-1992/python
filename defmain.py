4e10aa6e7f35462591233cd5d2362d2b2328ed11
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-04-28 08:24:41 -0400

    defmain()

---
+++ defmain.py
@@ -0,0 +1,9 @@
+
+def main(): # put this as main function so no need of proto type 
+    for i in range(3):
+       meow()    
+
+def meow():
+    print("meow")
+
+main() # need to call the main function 
