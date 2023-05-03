9fdbb1d63a56e6710c7ffa07fb25a9073b76a7da
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-05-03 11:33:06 -0400

    python try except

---
+++ try_except.py
@@ -0,0 +1,16 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+
+def get_height():
+   while True:
+    try: 
+        n=int (input("height:"))
+        if n > 0:
+            return n  
+    except ValueError:
+       print("not an integer")
+
+main()
